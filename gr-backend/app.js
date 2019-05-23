72d562b1a9a28c61cba2baa4b38afa9056772c53
lkakitani <leonardo.kakitani@gmail.com>
2019-05-23 16:36:29 -0300

    First backend commit

---
+++ gr-backend/app.js
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(3030);
+console.log('app running on port 3030');
---
+++ gr-backend/build/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.listen(3030);
+console.log('app running on port 3030');
