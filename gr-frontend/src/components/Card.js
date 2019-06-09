65f2aeaf65939722c5dd05ce3b2f609a7d90df04
lkakitani <leonardo.kakitani@gmail.com>
2019-06-09 14:38:55 -0300

    CSS file

--- gr-frontend/src/components/Card.js
+++ gr-frontend/src/components/Card.js
@@ -3,13 +3,15 @@ import React from 'react';
 export const Card = props => {
   return (
     <div className="ui card">
-      <div className="content">
+      <div className="content center aligned">
         <div className="header">{props.item.name}</div>
-        <div className="meta">{props.item.quote}</div>
         {/* <div className="description">
           Elliot Fu is a film-maker from New York.
         </div> */}
-        <img class="ui mini image" src={props.item.icon_path}></img>
+        <div className="img-card">
+          <img class="ui mini image" src={props.item.icon_path} alt={props.item.name}></img>
+        </div>
+        <div className="description">"{props.item.quote}"</div>
 
       </div>
       <div className="ui bottom attached button">
@@ -22,7 +24,7 @@ export const Card = props => {
 export const RerollCard = () => {
   return (
     <div className="ui card">
-      <div className="content">
+      <div className="content center aligned">
         <div className="header">?</div>
         <div className="description">
           I don't know / can't decide
