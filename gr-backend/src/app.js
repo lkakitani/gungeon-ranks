fc8c084f11df9c9f17bfeb37a8f037012f1acb65
lkakitani <leonardo.kakitani@gmail.com>
2019-07-15 21:21:29 -0300

    Remove cors

--- gr-backend/src/app.js
+++ gr-backend/src/app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/rankRoutes');
-// todo remove cors
-const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 routes(app);
 
